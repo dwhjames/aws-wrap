a247ecd8d31353c8ecd567ed189e678c3f51133d
Erwan Loisant <eloisant@gmail.com>
2012-10-03 17:36:22 +0200

    SimpleDB test

---
+++ simpledb/src/test/scala/simpledb.scala
@@ -0,0 +1,22 @@
+package aws.simpledb
+
+import scala.concurrent.Future
+import play.api.libs.ws._
+import play.api.libs.ws.WS._
+import aws.core._
+
+import org.specs2.mutable._
+
+object SimpleDBSpec extends Specification {
+
+  import 
+  
+  "SimpleDB API" should {
+    "Create a domain" in {
+      SimpleDB.
+    }
+  }
+
+}
+
+
