226a7378adbdb3c997abbfa0c4283b447fb1bb79
Erwan Loisant <eloisant@gmail.com>
2012-10-17 17:11:41 +0200

    Some scaladoc

--- dynamodb/src/main/scala/dynamodb.scala
+++ dynamodb/src/main/scala/dynamodb.scala
@@ -45,6 +45,14 @@ object DynamoDB {
     request(operation, body).map(r => tryParse[T](r))
   }
 
+  /**
+   * Returns a sequence of all the tables names associated with the current account and endpoint.
+   *
+   * The ListTables operation returns all of the table names associated with the account making the request, for the endpoint that receives the request.
+   *
+   * @param limit A number of maximum table names to return
+   * @param exclusiveStartTableName The name of the table that starts the list. If you already ran a ListTables operation and received an LastEvaluatedTableName value in the response, use that value here to continue the list.
+   */
   def listTables(limit: Option[Int] = None,
                  exclusiveStartTableName: Option[String] = None)(implicit region: AWSRegion): Future[SimpleResult[Seq[String]]] = {
     val data = (
@@ -53,6 +61,19 @@ object DynamoDB {
     post[Seq[String]]("ListTables", Json.toJson(data))
   }
 
+  /**
+   * The CreateTable operation adds a new table to your account. The table name must be unique among those associated with the AWS Account issuing the request,
+   * and the AWS region that receives the request (such as dynamodb.us-east-1.amazonaws.com).
+   *
+   * The CreateTable operation triggers an asynchronous workflow to begin creating the table. Amazon DynamoDB immediately returns the state of the table ([[Status.CREATING CREATING]])
+   * until the table is in the [[Status.ACTIVE ACTIVE]] state. Once the table is in the [[Status.ACTIVE ACTIVE]] state, you can perform data plane operations.
+   * Use [[describeTable]] to check the status of the table.
+   *
+   * @param tableName The name of the table to create.
+   *                  Allowed characters are a-z, A-Z, 0-9, '_' (underscore), '-' (dash), and '.' (dot).
+   *                  Names can be between 3 and 255 characters long.
+   * @param keySchema the primary key structure for the table. See [[PrimaryKey]] for more information.
+   */
   def createTable(tableName: String,
                   keySchema: PrimaryKey,
                   provisionedThroughput: ProvisionedThroughput)(implicit region: AWSRegion): Future[SimpleResult[TableDescription]] = {
@@ -63,6 +84,20 @@ object DynamoDB {
     post[TableDescription]("CreateTable", body)
   }
 
+  /**
+   * Updates the provisioned throughput for the given table. Setting the throughput for a table
+   * helps you manage performance and is part of the provisioned throughput feature of Amazon DynamoDB.
+   * For more information, see [[http://docs.amazonwebservices.com/amazondynamodb/latest/developerguide/WorkingWithDDTables.html#ProvisionedThroughput Specifying Read and Write Requirements (Provisioned Throughput)]].
+   *
+   * The provisioned throughput values can be upgraded or downgraded based on the maximums and minimums listed in Limits in Amazon DynamoDB.
+   *
+   * The table must be in the [[Status.ACTIVE ACTIVE]] state for this operation to succeed.
+   * UpdateTable is an asynchronous operation; while executing the operation, the table is in the [[Status.UPDATING UPDATING]] state.
+   * While the table is in the [[Status.UPDATING UPDATING]] state, the table still has the provisioned throughput from before the call.
+   * The new provisioned throughput setting is in effect only when the table returns to the [[Status.ACTIVE ACTIVE]] state after the UpdateTable operation.
+   *
+   * @param tableName
+   */
   def updateTable(tableName: String,
                   provisionedThroughput: ProvisionedThroughput)(implicit region: AWSRegion): Future[SimpleResult[TableDescription]] = {
     val body = Json.obj(
@@ -71,16 +106,50 @@ object DynamoDB {
     post[TableDescription]("UpdateTable", body)
   }
 
+  /**
+   * The DeleteTable operation deletes a table and all of its items.
+   * After a DeleteTable request, the specified table is in the [[Status.DELETING DELETING]] state until Amazon DynamoDB completes
+   * the deletion.
+   *
+   *  - If the table is in the [[Status.ACTIVE ACTIVE]] state, you can delete it.
+   *  - If a table is in [[Status.CREATING CREATING]] or [[Status.UPDATING UPDATING]] states,
+   * then Amazon DynamoDB returns a ResourceInUseException error.
+   *  - If the specified table does not exist, Amazon DynamoDB returns a ResourceNotFoundException.
+   *  - If table is already in the [[Status.DELETING DELETING]] state, no error is returned.
+   *
+   * Amazon DynamoDB might continue to accept data plane operation requests, such as [[getItem]] and [[putItem]],
+   * on a table in the DELETING state until the table deletion is complete.
+   *
+   * @param tableName
+   */
   def deleteTable(tableName: String)(implicit region: AWSRegion): Future[EmptySimpleResult] = {
     val body = Json.obj("TableName" -> tableName)
     post[Unit]("DeleteTable", body)
   }
 
+  /**
+   * Returns information about the table, including the current status of the table,
+   * the primary key schema and when the table was created.
+   *
+   * DescribeTable results are eventually consistent.
+   * If you use DescribeTable too early in the process of creating a table, Amazon DynamoDB returns a ResourceNotFoundException.
+   * If you use DescribeTable too early in the process of updating a table, the new values might not be immediately available.
+   *
+   * @param tableName
+   */
   def describeTable(tableName: String)(implicit region: AWSRegion): Future[SimpleResult[TableDescription]] = {
     val body = Json.obj("TableName" -> JsString(tableName))
     post[TableDescription]("DescribeTable", body)
   }
 
+  /**
+   * Creates a new item, or replaces an old item with a new item (including all the attributes).
+   * If an item already exists in the specified table with the same primary key, the new item completely replaces the existing item. You can perform a conditional put (insert a new item if one with the specified primary key doesn't exist), or replace an existing item if it has certain attribute values.
+   *
+   * @param tableName
+   * @param item A map of the attributes for the item, and must include the primary key values that define the item.
+   * Other attribute name-value pairs can be provided for the item. For more information about primary keys, see [[PrimaryKey]].
+   */
   def putItem(tableName: String,
               item: Map[String, DDBAttribute],
               expected: Map[String, Expected] = Map.empty,
--- dynamodb/src/main/scala/models/models.scala
+++ dynamodb/src/main/scala/models/models.scala
@@ -32,6 +32,14 @@ case class BinaryKey(attribute: String) extends KeySchemaElement {
   val typeCode = "B"
 }
 
+/**
+ * When you create a table, in addition to the table name, you must specify the primary key of the table. Amazon DynamoDB supports the following two types of primary keys:
+ *
+ *  - '''Hash Type Primary Key''' - Implemented by [[HashKey]]. In this case the primary key is made of one attribute, a hash attribute. Amazon DynamoDB builds an unordered hash index on this primary key attribute. In the preceding example, the ProductCatalog has Id as its primary key. It is the hash attribute.
+ *  - '''Hash and Range Type Primary Key''' - Implemented by [[CompositeKey]]. In this case, the primary key is made of two attributes. The first attributes is the hash attribute and the second one is the range attribute. Amazon DynamoDB builds an unordered hash index on the hash primary key attribute and a sorted range index on the range primary key attribute. For example, Amazon Web Services maintain several forums (see Discussion Forums). Each forum has many threads of discussion and each thread has many replies. You can potentially model this by creating the following three tables:
+ *
+ * More details: [[http://docs.amazonwebservices.com/amazondynamodb/latest/developerguide/DataModel.html#DataModelPrimaryKey]]
+ */
 sealed trait PrimaryKey {
   def hashKey: KeySchemaElement
 }
