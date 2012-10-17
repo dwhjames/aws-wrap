cd550ef4afe77651482fd6dc230df106e3709c61
Erwan Loisant <eloisant@gmail.com>
2012-10-17 17:36:33 +0200

    Simplify AttributeConverter

--- dynamodb/src/main/scala/models/AttributeConverter.scala
+++ dynamodb/src/main/scala/models/AttributeConverter.scala
@@ -1,79 +1,72 @@
 package aws.dynamodb
 
-trait AttributeConverter[T] {
-  def convert(attribute: DDBAttribute): Option[T]
+import scala.annotation.implicitNotFound
+
+@implicitNotFound("I don't know how to convert a DDBAttribute to a ${To}. Provide an implicit AttributeConverter for this type.")
+trait AttributeConverter[To] {
+  def convert(attribute: DDBAttribute): Option[To]
 }
 
 object AttributeConverter {
 
-  implicit val stringConvert = new AttributeConverter[String] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBString(s) => Some(s)
-      case _ => None
-    }
+  def apply[A](f: DDBAttribute => Option[A]): AttributeConverter[A] = new AttributeConverter[A] {
+    def convert(attribute: DDBAttribute) = f(attribute)
   }
 
-  implicit val doubleConvert = new AttributeConverter[Double] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBNumber(n) => Some(n)
-      case _ => None
-    }
-  }
 
-  implicit val longConvert = new AttributeConverter[Long] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBNumber(n) => Some(n.toLong)
-      case _ => None
-    }
-  }
+  implicit val stringConvert = AttributeConverter[String](_ match {
+    case DDBString(s) => Some(s)
+    case _ => None
+  })
 
-  implicit val binaryConvert = new AttributeConverter[Array[Byte]] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBBinary(b) => Some(b)
-      case _ => None
-    }
-  }
+  implicit val doubleConvert = AttributeConverter[Double](_ match {
+    case DDBNumber(n) => Some(n)
+    case _ => None
+  })
 
-  implicit val stringSetConvert = new AttributeConverter[Set[String]] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBStringSet(ss) => Some(ss)
-      case _ => None
-    }
-  }
+  implicit val longConvert = AttributeConverter[Long](_ match {
+    case DDBNumber(n) => Some(n.toLong)
+    case _ => None
+  })
 
-  implicit val doubleSetConvert = new AttributeConverter[Set[Double]] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBNumberSet(ns) => Some(ns)
-      case _ => None
-    }
-  }
+  implicit val intConvert = AttributeConverter[Int](_ match {
+    case DDBNumber(n) => Some(n.toInt)
+    case _ => None
+  })
 
-  implicit val binarySetConvert = new AttributeConverter[Set[Array[Byte]]] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBBinarySet(bs) => Some(bs)
-      case _ => None
-    }
-  }
+  implicit val binaryConvert = AttributeConverter[Array[Byte]](_ match {
+    case DDBBinary(b) => Some(b)
+    case _ => None
+  })
 
-  implicit val stringSeqConvert = new AttributeConverter[Seq[String]] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBStringSet(ss) => Some(ss.toSeq)
-      case _ => None
-    }
-  }
+  implicit val stringSetConvert = AttributeConverter[Set[String]](_ match {
+    case DDBStringSet(ns) => Some(ns)
+    case _ => None
+  })
 
-  implicit val doubleSeqConvert = new AttributeConverter[Seq[Double]] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBNumberSet(ns) => Some(ns.toSeq)
-      case _ => None
-    }
-  }
+  implicit val doubleSetConvert = AttributeConverter[Set[Double]](_ match {
+    case DDBNumberSet(ns) => Some(ns)
+    case _ => None
+  })
 
-  implicit val binarySeqConvert = new AttributeConverter[Seq[Array[Byte]]] {
-    override def convert(attribute: DDBAttribute) = attribute match {
-      case DDBBinarySet(bs) => Some(bs.toSeq)
-      case _ => None
-    }
-  }
+  implicit val binarySetConvert = AttributeConverter[Set[Array[Byte]]](_ match {
+    case DDBBinarySet(bs) => Some(bs)
+    case _ => None
+  })
+
+  implicit val stringSeqConvert = AttributeConverter[Seq[String]](_ match {
+    case DDBStringSet(ss) => Some(ss.toSeq)
+    case _ => None
+  })
+
+  implicit val doubleSeqConvert = AttributeConverter[Seq[Double]](_ match {
+    case DDBNumberSet(ns) => Some(ns.toSeq)
+    case _ => None
+  })
+
+  implicit val binarySeqConvert = AttributeConverter[Seq[Array[Byte]]](_ match {
+    case DDBBinarySet(bs) => Some(bs.toSeq)
+    case _ => None
+  })
 
 }
