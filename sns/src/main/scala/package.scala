8acb1b406f87322a01f093b3393826233972d43c
Erwan Loisant <eloisant@gmail.com>
2012-10-30 15:49:20 +0100

    Doc

---
+++ sns/src/main/scala/package.scala
@@ -0,0 +1,12 @@
+package aws
+
+/**
+ * Access to Amazon [[http://aws.amazon.com/documentation/sns/ Simple Notification Service (SNS)]].
+ *
+ * Amazon Simple Notification Service is a web service that enables you to build distributed web-enabled applications.
+ * Applications can use Amazon SNS to easily push real-time notification messages to interested subscribers over multiple
+ * delivery protocols. 
+ *
+ * See the [[SNS]] object for available operations.
+ */
+package object sns {}
