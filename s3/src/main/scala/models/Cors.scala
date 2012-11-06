d97a31f2d00b2ad9d9f9d733582be263fd53699f
jtournay <jtournay@linkedin.com>
2012-11-06 07:05:19 -0800

    S3: Cors scaladoc

--- s3/src/main/scala/models/Cors.scala
+++ s3/src/main/scala/models/Cors.scala
@@ -16,6 +16,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 import aws.s3.S3.HTTPMethods._
 
+/**
+* Cross-Origin Resource Sharing rule
+* @see http://docs.amazonwebservices.com/AmazonS3/latest/dev/cors.html
+*/
 case class CORSRule(origins: Seq[String] = Nil,
                     methods: Seq[Method] = Nil,
                     headers: Seq[String] = Nil,
@@ -28,6 +32,14 @@ object CORSRule {
   import Permisions._
   import ACLs._
 
+  /**
+  * Sets the Cross-Origin Resource Sharing configuration for your bucket.
+  * If the configuration exists, Amazon S3 replaces it.
+  * To use this operation, you must be allowed to perform the s3:PutCORSConfiguration action. By default, the bucket owner has this permission and can grant it to others.
+  * You can add up to 100 rules to the configuration
+  * @param bucketname The name of the bucket you want to set cors on.
+  * @param rules List of CORSRule to apply on this Bucket
+  */
   def create(bucketName: String, rules: CORSRule*) = {
     val b =
       <CORSConfiguration>
@@ -50,9 +62,19 @@ object CORSRule {
       parameters = ps)
   }
 
+  /**
+  * Returns the cors configuration information set for the bucket.
+  * To use this operation, you must have permission to perform the s3:GetCORSConfiguration action. By default, the bucket owner has this permission and can grant it to others.
+  * @param bucketname The name of the bucket.
+  */
   def get(bucketName: String) =
     Http.get[Seq[CORSRule]](Some(bucketName), subresource = Some("cors"))
 
+  /**
+  * Deletes the cors configuration information set for the bucket.
+  * To use this operation, you must have permission to perform the s3:PutCORSConfiguration action. The bucket owner has this permission by default and can grant this permission to others.
+  * @param bucketname The name of the bucket you want to delete.
+  */
   def delete(bucketName: String) =
     Http.delete[Unit](Some(bucketName), subresource = Some("cors"))
 }
