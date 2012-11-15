f1b4700c1a343941075e10f1a0a211866988f4d1
Erwan Loisant <eloisant@gmail.com>
2012-11-15 15:04:27 +0100

    Fix empty test

--- dynamodb/src/test/java/com/pellucid/aws/dynamodb/DynamoDBTest.java
+++ dynamodb/src/test/java/com/pellucid/aws/dynamodb/DynamoDBTest.java
@@ -1,5 +1,14 @@
 package com.pellucid.aws.dynamodb;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import com.pellucid.aws.results.*;
+
 public class DynamoDBTest {
+
 
 }
