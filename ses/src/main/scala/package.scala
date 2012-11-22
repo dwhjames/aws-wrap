b763dc6b6fa26bedb5c583d636343191a598d1b0
Erwan Loisant <eloisant@gmail.com>
2012-11-22 13:21:01 +0100

    Fix comment

--- ses/src/main/scala/package.scala
+++ ses/src/main/scala/package.scala
@@ -3,8 +3,6 @@ package aws
 /**
  * Access to Amazon [[http://aws.amazon.com/documentation/ses/ SES]].
  *
- * DynamoDB is a NoSQL database, more advanced than [[aws.ses SES]].
- *
  * See the [[SES]] object for available operations.
  */
 package object ses {}
