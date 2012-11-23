07f6f298f478ce0735c3aeb89d9c4850139e1703
Erwan Loisant <eloisant@gmail.com>
2012-11-23 10:13:50 +0100

    Code cleanup

--- core/src/main/scala/core/signature/SignerEncoder.scala
+++ core/src/main/scala/core/signature/SignerEncoder.scala
@@ -11,26 +11,28 @@ object SignerEncoder {
     // encode everything except what is included in the bitset
     val dontEncode = new BitSet(256)
     (Range.inclusive('a', 'z') ++ Range.inclusive('A', 'Z') ++ Range.inclusive('0', '9')).foreach(dontEncode.set(_))
-    Seq('-', '_' , '.').foreach(dontEncode.set(_))
+    Seq('-', '_', '.').foreach(dontEncode.set(_))
     dontEncode
   }
 
   def encode(str: String): String = {
     val lowerDiff = 'a' - 'A';
-    str.getBytes("UTF-8").foldLeft("") { (r, c) => c match {
-      case c if dontEncode.get(c) => r + c.toChar
-      case c => {
-        val ch0 = Character.forDigit((c.toChar >> 4) & 0xf, 16).toInt match {
-          case letter if Character.isLetter(letter) => letter - lowerDiff
-          case other => other
+    str.getBytes("UTF-8").foldLeft("") { (r, c) =>
+      c match {
+        case c if dontEncode.get(c) => r + c.toChar
+        case c => {
+          val ch0 = Character.forDigit((c.toChar >> 4) & 0xf, 16).toInt match {
+            case letter if Character.isLetter(letter) => letter - lowerDiff
+            case other => other
+          }
+          val ch1 = Character.forDigit(c.toChar & 0xf, 16).toInt match {
+            case letter if Character.isLetter(letter) => letter - lowerDiff
+            case other => other
+          }
+          r + "%" + ch0.toChar + ch1.toChar
         }
-        val ch1 = Character.forDigit(c.toChar & 0xf, 16).toInt match {
-          case letter if Character.isLetter(letter) => letter - lowerDiff
-          case other => other
-        }
-        r + "%" + ch0.toChar + ch1.toChar
       }
-    }}
+    }
   }
 
 }
