32bc33b286172175fab6d3445d1f3b715832009f
Erwan Loisant <eloisant@gmail.com>
2012-12-07 16:53:26 +0100

    Progress on SNS Java API testing

--- sns/src/test/java/com/pellucid/aws/sns/SNSTest.java
+++ sns/src/test/java/com/pellucid/aws/sns/SNSTest.java
@@ -25,4 +25,41 @@ public class SNSTest {
         assertTrue(result2.toString(), result2.isSuccess());
     }
 
+    @Test
+    public void listTopics() throws Exception {
+        Result<SNSMeta, String> result = Await.result(sns.createTopic("java-list-topic"), timeout);
+        assertTrue(result.toString(), result.isSuccess());
+        String topicArn = result.body();
+
+        Result<SNSMeta, ListTopics> listTopicsResults = Await.result(sns.listTopics(), timeout);
+        assertTrue(listTopicsResults.toString(), listTopicsResults.isSuccess());
+        boolean found = false;
+        for (String topic: listTopicsResults.body().topics()) {
+            if (topic.equals(topicArn)) found = true;
+        }
+        assertTrue("Couldn't find the newly created topic in the list", found);
+
+        Result<SNSMeta, Object> result2 = Await.result(sns.deleteTopic(topicArn), timeout);
+        assertTrue(result2.toString(), result2.isSuccess());
+    }
+
+    @Test
+    public void subscribe() throws Exception {
+        Result<SNSMeta, String> result = Await.result(sns.createTopic("java-subscribe"), timeout);
+        assertTrue(result.toString(), result.isSuccess());
+        String topicArn = result.body();
+
+        Result<SNSMeta, String> subscribeRes = Await.result(sns.createTopic("java-subscribe"), timeout);
+        assertTrue(subscribeRes.toString(), subscribeRes.isSuccess());
+
+        Result<SNSMeta, Object> result2 = Await.result(sns.deleteTopic(topicArn), timeout);
+        assertTrue(result2.toString(), result2.isSuccess());
+    }
+
+    // Publish
+
+    // Add / remove permissions
+
+    // Set attributes
+
 }
