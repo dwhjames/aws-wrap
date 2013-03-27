e7a63355ecfeed95f1c235b7a09fe1a1d42cbdb6
Daniel James <daniel.james@pellucid.com>
2013-03-26 20:05:59 -0400

    minor: tidy up s3 permissions

--- s3/src/main/scala/permissions.scala
+++ s3/src/main/scala/permissions.scala
@@ -3,28 +3,30 @@ package aws.s3
 object Permissions {
 
   object Grantees {
-    sealed class Grantee(n: String, v: String) {
-      val name = n
-      val value = v
+
+    sealed trait Grantee {
+      val name:  String
+      val value: String
     }
-    object Grantee {
-      def apply(name: String, value: String) = new Grantee(name, value)
-      def unapply(g: Grantee): Option[(String, String)] = Some((g.name, g.value))
-    }
-    case class Email(override val value: String) extends Grantee("emailAddress", value)
-    case class Id(override val value: String) extends Grantee("id", value)
-    case class Uri(override val value: String) extends Grantee("uri", value)
+
+    case class Email(override val value: String) extends Grantee { override val name = "emailAddress" }
+    case class Id   (override val value: String) extends Grantee { override val name = "id" }
+    case class Uri  (override val value: String) extends Grantee { override val name = "uri" }
   }
 
-  def X_AMZ_ACL(acl: CannedACL.Value) = ("x-amz-acl" -> acl.toString)
+  def X_AMZ_ACL(acl: CannedACL.Value) =
+    ("x-amz-acl" -> acl.toString)
 
   import Grantees._
-  private def s(gs: Seq[Grantee]) = gs.map { case Grantee(n, v) => "%s=\"%s\"".format(n, v) }.mkString(", ")
+  private def s(gs: Seq[Grantee]) =
+    gs map { g =>
+      "%s=\"%s\"".format(g.name, g.value)
+    } mkString (", ")
 
   type Grant = (String, String)
-  def GRANT_READ(gs: Grantee*): Grant = "x-amz-grant-read" -> s(gs)
-  def GRANT_WRITE(gs: Grantee*): Grant = "x-amz-grant-write" -> s(gs)
-  def GRANT_READ_ACP(gs: Grantee*): Grant = "x-amz-grant-read-acp" -> s(gs)
-  def GRANT_WRITE_ACP(gs: Grantee*): Grant = "x-amz-grant-write-acp" -> s(gs)
+  def GRANT_READ        (gs: Grantee*): Grant = "x-amz-grant-read"         -> s(gs)
+  def GRANT_WRITE       (gs: Grantee*): Grant = "x-amz-grant-write"        -> s(gs)
+  def GRANT_READ_ACP    (gs: Grantee*): Grant = "x-amz-grant-read-acp"     -> s(gs)
+  def GRANT_WRITE_ACP   (gs: Grantee*): Grant = "x-amz-grant-write-acp"    -> s(gs)
   def GRANT_FULL_CONTROL(gs: Grantee*): Grant = "x-amz-grant-full-control" -> s(gs)
 }
