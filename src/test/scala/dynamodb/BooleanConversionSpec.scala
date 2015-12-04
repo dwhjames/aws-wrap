ba43507833a2e0e4727befc3cc3b8b4f5d7585a5
janlisse <janlisse@example.com>
2015-12-04 22:05:24 +0100

    fix boolean conversion

---
+++ src/test/scala/dynamodb/BooleanConversionSpec.scala
@@ -0,0 +1,24 @@
+package dynamodb
+
+import com.github.dwhjames.awswrap.dynamodb._
+import scala.collection.JavaConverters._
+import org.scalatest.{FlatSpec, Matchers}
+
+class BooleanConversionSpec extends FlatSpec with Matchers {
+
+
+  it should "convert from DynamoDb Bool to Scala Boolean" in {
+
+    val attributeValue = new AttributeValue().withBOOL(false)
+    val bool = attributeValueToBoolean(attributeValue)
+    bool shouldBe false
+  }
+
+  it should "convert from DynamoDb Bool Collection to Scala Boolean Set" in {
+
+    val attributeValue = new AttributeValue().withBOOL(false)
+    val attributeValueList = new AttributeValue().withL(Seq(attributeValue).asJava)
+    val bool = attributeValueToBooleanSet(attributeValueList)
+    bool shouldBe Set(false)
+  }
+}
