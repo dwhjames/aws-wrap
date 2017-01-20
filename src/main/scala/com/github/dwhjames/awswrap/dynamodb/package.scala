acfb4181ea68383ac2eb247a1b7d29e3616ce4d0
Tim Wilkins <glidester@gmail.com>
2017-01-20 16:52:17 +0000

    Reversing out the change in commit SHA ba435078 by janlisse, 04/12/2015 21:05 parent 53eb5522. Only the reading of Booleans was changed to the v2 Schema getBOOL while the writing function remained using the withS method. This resulted in a NullPointerException.
    Furthermore, after discussion with @mingchuno it was agreed that the implicit conversions in package.scala should remain as Dynamo Schema v1 (which does not have the BOOL or L data types). To access the v2 Schema types user can utilize the new com.github.dwhjames.awswrap.dynamodb.v2.DataTypeConversions instead.

--- src/main/scala/com/github/dwhjames/awswrap/dynamodb/package.scala
+++ src/main/scala/com/github/dwhjames/awswrap/dynamodb/package.scala
@@ -154,9 +154,9 @@ package object dynamodb extends QueryBuilder {
   implicit val attributeValueToByteArraySet = (x: AttributeValue) => catchAndRethrowConversion { x.getBS.asScala.map(_.array).toSet }
 
   /** string AttributeValue to Boolean */
-  implicit val attributeValueToBoolean = (x: AttributeValue) => catchAndRethrowConversion { x.getBOOL }
+  implicit val attributeValueToBoolean = (x: AttributeValue) => catchAndRethrowConversion { x.getS.toBoolean }
   /** string set AttributeValue to Set[Boolean] */
-  implicit val attributeValueToBooleanSet = (x: AttributeValue) => catchAndRethrowConversion { x.getL.asScala.map(_.getBOOL).toSet }
+  implicit val attributeValueToBooleanSet = (x: AttributeValue) => catchAndRethrowConversion { x.getSS.asScala.map(_.toBoolean).toSet }
 
   /** string AttributeValue to BigInt */
   implicit val attributeValueToBigInt     = (x: AttributeValue) => catchAndRethrowConversion { BigInt(x.getS) }
